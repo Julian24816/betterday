1586f4c20c311a3b61d38c2dda66f501f00e6236
Julian Müller <julian.mueller@stud-mail.uni-wuerzburg.de>
2018-12-28 14:36:06 +0100

    clean up tests

--- test/de/julian/betterday/data/time/TimeIntervalTest.java
+++ test/de/julian/betterday/data/time/TimeIntervalTest.java
@@ -11,31 +11,28 @@ import static org.junit.jupiter.api.Assertions.*;
 class TimeIntervalTest {
 
     @Test
-    void testStringRepresentation() {
-        assertStringRepresentation("TimeInterval(2018-12-27 13:00 - 14:40)", 13, 0, 14, 40);
-    }
-
-    @Test
     void testArgumentPermutationDoesNotMatter() {
-        assertStringRepresentation("TimeInterval(2018-12-27 12:00 - 13:00)", 13, 0, 12, 0);
-        assertStringRepresentation("TimeInterval(2018-12-27 12:00 - 13:00)", 12, 0, 13, 0);
+        final String expected = "12:00 - 13:00";
+        assertCorrectCreation(expected, 13, 0, 12, 0);
+        assertCorrectCreation(expected, 12, 0, 13, 0);
     }
 
     @Test
-    void testAccuracyIsUsedForComparison() {
-        assertStringRepresentation("TimeInterval(2018-12-27 12:00 - 12:50)", 11, 56, 12, 53);
-        assertStringRepresentation("TimeInterval(2018-12-27 12:00 - 12:50)", 12, 4, 12, 48);
+    void testAccuracyRounding() {
+        assertCorrectCreation("11:50 - 12:40", 11, 54, 12, 44);
 
-        assertStringRepresentation("TimeInterval(2018-12-27 11:50 - 12:40)", 11, 54, 12, 44);
-        assertStringRepresentation("TimeInterval(2018-12-27 12:10 - 13:00)", 12, 6, 12, 57);
+        assertCorrectCreation("12:00 - 12:50", 11, 55, 12, 45);
+        assertCorrectCreation("12:00 - 12:50", 12, 4, 12, 54);
+        
+        assertCorrectCreation("12:10 - 13:00", 12, 5, 12, 55);
     }
 
-    private void assertStringRepresentation(String expectedStringRepresentation, int hour1, int minute1, int hour2, int minute2) {
+    private void assertCorrectCreation(String expected, int hour1, int minute1, int hour2, int minute2) {
         TimeInterval timeInterval = new TimeInterval(
                 getDateForHourAndMinute(hour1, minute1),
                 getDateForHourAndMinute(hour2, minute2)
         );
-        assertEquals(expectedStringRepresentation, timeInterval.toString());
+        assertEquals("TimeInterval(2018-12-27 " + expected + ")", timeInterval.toString());
     }
 
     private Date getDateForHourAndMinute(int hour, int minute) {
