518ed7f02f94b1b5f045be999091ee4208ab9045
Julian Müller <julian.mueller@stud-mail.uni-wuerzburg.de>
2018-12-30 20:57:17 +0100

    clean up Parse Exception class

--- src/de/julian/betterday/data/time/TimeIntervalParseException.java
+++ src/de/julian/betterday/data/time/TimeIntervalParseException.java
@@ -1,12 +1,7 @@
 package de.julian.betterday.data.time;
 
-import java.text.ParseException;
-
 class TimeIntervalParseException extends Exception {
     TimeIntervalParseException(String message) {
         super(message);
-    }
-    TimeIntervalParseException(String message, Throwable reason) {
-        super(message, reason);
     }
 }
