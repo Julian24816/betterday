ff08a560cacefa5008ba85a362bb44bb8b88dbb0
Julian Müller <julian.mueller@stud-mail.uni-wuerzburg.de>
2018-12-30 20:56:36 +0100

    add test for add command parser

---
+++ test/de/julian/betterday/app/commandlineapp/add/AddCommandParserTest.java
@@ -0,0 +1,36 @@
+package de.julian.betterday.app.commandlineapp.add;
+
+import de.julian.betterday.app.commandlineapp.Command;
+import de.julian.betterday.app.commandlineapp.controller.CommandParseException;
+import de.julian.betterday.data.activity.ActivityBuilder;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AddCommandParserTest {
+
+    private AddCommandParser parser;
+
+    @BeforeEach
+    void setUp() {
+        parser = new AddCommandParser();
+    }
+
+    @Test
+    void testSimpleParsing() {
+        try {
+            Command actual = parser.parse(new String[]{"add", "sleep", "12:00", "13:00"});
+            Command expected = new ConfirmAddCommand(
+                    new ActivityBuilder()
+                            .setCategory("sleep")
+                            .setStartTime("12:00")
+                            .setEndTime("13:00")
+                            .build());
+            assertEquals(expected, actual);
+        } catch (CommandParseException e) {
+            fail("parsing resulted in an exception", e);
+        }
+    }
+
+}
